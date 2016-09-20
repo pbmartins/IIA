c4d70b6ebb1121bd0979dc3c6dee87cdec575a95
Pedro Martins <pbmartins@ua.pt>
2016-09-20 19:26:38 +0100

    Added new exercices in g04.py

--- L_01/G_02/g04.py
+++ L_01/G_02/g04.py
@@ -1,5 +1,6 @@
 import sys
 import math
+from functools import reduce
 
 def func(f, x):
     return f(x)
@@ -33,6 +34,38 @@ def func9(f, lst):
         return None
     ## Incomplete
 
+# map()
+def apply(f, lst):
+    return [] if lst == [] else [f(lst[0])] + apply(f, lst[1:])
+
+# filter()
+def filt(f, lst):
+    if lst == []:
+        return []
+    if f(lst[0]):
+        return [lst[0]] + filt(f, lst[1:])
+    return filt(f, lst[1:])
+
+# reduce()
+def reduc(f, lst, null):
+    return null if lst == [] else f(lst[0], reduc(f, lst[1:], null))
+
+def conc_aplic(f, lst):
+    return [] if lst == [] else list(map(lambda x : f(x), lst[0])) + conc_aplic(f, lst[1:])
+    # return [] if lst == [] else [f(x) for x in lst[0]] + conc_aplic(f, lst[1:])
+
+def aplic_combin(f, lst_1, lst_2):
+    if lst_1 == [] and lst_2 == []:
+        return []
+    elif lst_1 == [] or lst_2 == []:
+        return None
+    lst = aplic_combin(f, lst_1[1:], lst_2[1:])
+    return [f(lst_1[0], lst_2[0])] + lst if not lst == None else None
+
+def func16(f, lst, null):
+    return [] if lst == [] else [reduce(lambda x,s : f(x, s), lst[0], null)] + func16(f, lst[1:], null)
+    # return [] if lst == [] else [f(x) for x in lst[0]] + func16(f, lst[1:], null)
+
 print(func(lambda x : x%2 != 0, 3))
 print(func(lambda x : x < 0, -1))
 print(func2args(lambda x, y: abs(x) < abs(y), 4, -3))
@@ -41,3 +74,9 @@ print(func5(lambda x, y : x + y, lambda x, y : x * y, lambda x, y : x > y, 1, 2,
 print(func6(lambda x : x > 0, [1, 2, 3]))
 print(func7(lambda x : x < 0, [-1, 2, 3]))
 print(func8([1, 2], [1, 2, 3, 4]))
+
+
+
+print(conc_aplic(lambda x : x**2, [[1, 2, 3], [4, 5, 6]]))
+print(aplic_combin(lambda x,y : x*y, [1, 2, 3], [1, 2, 3, 4]))
+print(func16(lambda x,s : x * s, [[1, 2, 3], [4, 5, 6], [7, 8, 9]], 1))
